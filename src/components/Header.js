dcbd5aecf6cbc270d06ba535cc2a0bc620ec1eee
Danny Mackey <dmack928@gmail.com>
2018-11-28 15:38:20 -0500

    added botstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Header = () => {
-  return(
-    <div>
-      <h1>Contact Manager</h1>
+const Header = (props) => {
+  const { branding } = props
+  return (
+   <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+    <div className="container">
+      <a href="/" className="navbar-brand">
+        { branding }
+       </a>
+        }
     </div>
-
+    </nav>
   );
 
-}
+};
+
+Header.defaultProps = {
+  branding: 'My App'
+};
+
+Header.PropTypes ={
+  branding: PropTypes.string.isRequired
+};
+
+
 
 export default Header;
